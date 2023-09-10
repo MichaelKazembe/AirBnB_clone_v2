47d640b564786ef8781a7ac26bf21913d7e32b2a
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-10 15:31:56 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,19 @@
 #!/usr/bin/env bash
-# sets up web servers for the deployment of web_static
+# Script sets up web servers for the deployment of Web_static
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
-
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-# Create a fake HTML file
-echo "<html><head></head><body>Hello, web_static!</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create or recreate the symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-# Give ownership to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Configure Nginx
-config_file="/etc/nginx/sites-available/default"
-nginx_config="
-server {
-    listen 80;
-    server_name _;
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-
-    location / {
-        index index.html;
-        add_header X-Served-By $hostname;
-    }
-}
-"
-echo "$nginx_config" | sudo tee "$config_file" > /dev/null
-
-# Restart Nginx
-sudo service nginx restart
-
+apt update -y
+apt install nginx -y
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
