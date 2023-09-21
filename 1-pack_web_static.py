9cf2098f482d0af0158f6472b61b421848476c71
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-21 03:22:22 -0700

    archived

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,21 @@
-#!/usr/bin/python3
-"""generates an archive of web_static folder"""
+#!/usr/bin/python3 
+#creates a .tgz archive of the contents of the "web_static" folder and saves it in the "versions
 
+import tarfile
 from fabric.api import local
 from datetime import datetime
 import os
 
 
 def do_pack():
-    """function to generate the archive"""
+    """packs file """
     try:
-        # Create the 'versions' directory if it doesn't exist
-        if not os.path.exists('versions'):
-            os.makedirs('versions')
-
-        # Create the archive filename (web_static_<timestamp>.tgz)
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        archive_name = f'web_static_{timestamp}.tgz'
-
-        # Create the archive using tar command
-        local('tar -cvzf versions/{} web_static'.format(archive_name))
-
-        # Return the archive path if successful
-        return 'versions/{}'.format(archive_name)
-    except Exception as e:
+        name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S")
+        local('mkdir -p versions')
+        local("tar -cvzf versions/{}.tgz {}".format(
+            name, "web_static/"))
+        size = os.path.getsize("./versions/{}.tgz".format(name))
+        print("web_static packed: versions/{}.tgz -> {}Bytes".format(
+            name, size))
+    except:
         return None
