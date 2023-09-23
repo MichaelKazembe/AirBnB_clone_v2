82aec7fe4eb18e2c11f773fc5a0dc58f8c81df61
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-23 14:58:10 -0700

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    >A script that starts a Flask web application:
+    >Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB!”
+        /c/<text>: display “C followed by the value of text”
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """ Function that displays 'Hello HBNB' """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ Function that displays 'HBNB' """
+    return "HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """ Function that displays 'C followed by the value of text' """
+    return f'C {text}'
+
+
+if __name__ == '__main__':
+    app.run()
