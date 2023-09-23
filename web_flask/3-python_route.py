fa0e02cc4b2afa8aa0c3c51dcfa855e446b74939
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-23 16:42:21 -0700

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,9 +3,12 @@
     >A script that starts a Flask web application:
     >Routes:
         /: display “Hello HBNB!”
-        /hbnb: display “HBNB!”
-        /c/<text>: display “C followed by value of text”
-        /python/<text>: display “Python followed by value of text”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ”, followed by the value of the
+            text variable (replace underscore _ symbols with a space )
+        /python/<text>: display “Python ”, followed by the value of the
+            text variable (replace underscore _ symbols with a space )
+            The default value of text is “is cool”
 """
 from flask import Flask
 
@@ -21,18 +24,18 @@ def hello_route():
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """ Function that displays 'HBNB' """
-    return "HBNB!"
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def hello_c(text):
-    """ Function that displays 'C followed by value of text' """
+    """ Function that displays 'C ', followed by the value of the text """
     return f'C {text.replace("_", " ")}'
 
 
 @app.route('/python/<text>', strict_slashes=False)
-def hello_python(text="is cool"):
-    """ Function that displays 'Python followed by value of text' """
+def hello_python(text="is_cool"):
+    """ Function that displays 'Python ', followed by the value of the text """
     return f'Python {text.replace("_", " ")}'
 
 
