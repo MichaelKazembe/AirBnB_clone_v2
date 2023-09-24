ecc83174de2058e44f26f1693c1f6507799a3c51
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-24 10:42:32 -0700

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -33,6 +33,7 @@ def hello_c(text):
     return f'C {text.replace("_", " ")}'
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def hello_python(text="is_cool"):
     """ Function that displays 'Python ', followed by the value of the text """
